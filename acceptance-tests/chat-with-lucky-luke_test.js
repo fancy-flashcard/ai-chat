f9a997a99c08e3c218a38d9dd6605aa88534c4a7
Michael Spengler <mycamper@gmx.de>
2020-07-24 00:22:33 +0200

    walking skeleton with NLP = complete

--- acceptance-tests/chat-with-lucky-luke_test.js
+++ acceptance-tests/chat-with-lucky-luke_test.js
@@ -1,11 +1,11 @@
 Feature('chat-with-lucky-luke');
 
-Scenario('test something', (I) => {
+Scenario('test lucky luke is lucky', (I) => {
     I.amOnPage('https://fancy-chats.com:3443/')
     I.see('Fancy Chat')
     I.click('Fancy Chat')
     I.see('Lucky Luke')
     I.fillField('#input', 'Hi')
-    // I.wait(5)
-    // I.see('Hi')
+    I.wait(7)
+    I.see(':)')
 });
