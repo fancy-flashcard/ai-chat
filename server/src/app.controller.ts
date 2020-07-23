2d8a9e83104ce3dfdd9413858aa40ef1995c65f9
Michael Spengler <mycamper@gmx.de>
2020-07-23 20:51:41 +0200

    preparing students demo

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import * as fs from 'fs-sync'
+import * as path from 'path'
 
 @Controller()
 export class AppController {
+  private readonly baseHTML = fs.read(path.join(__dirname, './../../static-assets/index.html'))
+
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(@Res() res: any) {
+    res.send(this.baseHTML)
   }
 }
